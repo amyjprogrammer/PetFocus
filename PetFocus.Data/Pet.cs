d533e3bcf184e6e085ad8fbde00fc25d17fb5382
amyjprogrammer <amyjprogrammer@gmail.com>
2021-11-16 13:03:21 -0500

    added pet

---
+++ PetFocus.Data/Pet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFocus.Data
+{
+    public enum Species
+    {
+        Cat, Dog
+    }
+    public enum Sex
+    {
+        Male, Female
+    }
+
+    public class Pet
+    {
+        [Key]
+        public int PetId { get; set; }
+
+        [Required]
+        public string PetName { get; set; }
+
+        [Required]
+        public Species Species { get; set; }
+
+        [Required]
+        public Sex PetSex { get; set; }
+
+        public bool IsSpayedNeutered { get; set; }
+
+        [Required]
+        public string Breed { get; set; }
+
+        [Required]
+        public DateTimeOffset Birthdate { get; set; }
+
+        public string MicrochipNum { get; set; }
+
+        public string VetName { get; set; }
+
+        public virtual ICollection<Reminder> Reminders { get; set; } = new List<Reminder>();
+
+        public virtual ICollection<Weight> Weights { get; set; } = new List<Weight>();
+    }
+}
---
+++ PetFocus.Data/Reminder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFocus.Data
+{
+    public class Reminder
+    {
+        [Key]
+        public int ReminderId { get; set; }
+
+        [ForeignKey(nameof(Pet))]
+        public int PetId { get; set; }
+
+        public virtual Pet Pet { get; set; }
+
+        public DateTime HeartwormMed { get; set; }
+
+        public bool IsTimeForHeartMed
+        {
+            get
+            {
+                //heart meds need to be 30 days away
+                //would like to make that every month- stretch goal
+                var today = DateTime.Now;
+                var next = new DateTime(today.Year, HeartwormMed.Month, HeartwormMed.Day);
+
+                return ((next - today).Days == 30);
+            }
+        }
+
+        public DateTime RabiesVac { get; set; }
+
+        public bool IsThreeYearRabiesVac { get; set; }
+
+        public bool IsTimeForRabies 
+        { 
+            get 
+            {
+                var today = DateTime.Now;
+                var next = new DateTime(today.Year, RabiesVac.Month, RabiesVac.Day);
+            } 
+        }
+    }
+}
