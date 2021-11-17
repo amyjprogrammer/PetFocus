a1eeeb1fef6ff271d6a269ae3127bd8364b9c7fe
amyjprogrammer <amyjprogrammer@gmail.com>
2021-11-16 23:08:55 -0500

    controllers

---
+++ PetFocus.WebMVC/Controllers/HomemadeFoodController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetFocus.WebMVC.Controllers
+{
+    public class HomemadeFoodController : Controller
+    {
+        // GET: HomemadeFood
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
